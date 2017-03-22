fd7ae515edb01ec48e36b7e7600d7d80fc8a06ea
imifekete <imifekete@gmail.com>
2017-03-22 11:57:11 +0100

    exercise08

---
+++ week 02/day 03/src/DefineBasicInfo.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Fekete Imre on 2017.03.22..
+ */
+public class DefineBasicInfo {
+  public static void main(String[] args) {
+         String name = "Imre Fekete";
+         int age = 31;
+         double height = 1.65;
+         boolean isMarried = false;
+         System.out.println(name);
+         System.out.println(age);
+         System.out.println(height);
+         System.out.println(isMarried);
+  }
+}
