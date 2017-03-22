30513849153a8700528db010f771817a92727e7e
imifekete <imifekete@gmail.com>
2017-03-22 12:04:07 +0100

    exercise09

---
+++ week 02/day 03/src/Bmi.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Fekete Imre on 2017.03.22..
+ */
+public class Bmi {
+  public static void main(String[] args) {
+        double massInKg =  65.01;
+        double heightInM = 1.65;
+        System.out.println("BMI: " + (massInKg / (heightInM * heightInM)));
+  }
+}
