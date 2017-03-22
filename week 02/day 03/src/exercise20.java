f69f31c84befdf8aba0b68ab3904f7bedc07a595
imifekete <imifekete@gmail.com>
2017-03-22 15:05:22 +0100

    exercise20

---
+++ week 02/day 03/src/exercise20.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Fekete Imre on 2017.03.22..
+ */
+
+import java.util.Scanner;
+public class exercise20 {
+
+  public static void main(String[] args) {
+
+// Write a program that asks for two numbers and prints the bigger one
+    Scanner myScanner = new Scanner(System.in);
+        System.out.println("first number: ");
+        int a = myScanner.nextInt();
+        System.out.println("second  number: ");
+        int b = myScanner.nextInt();
+
+                if (a > b) {
+            System.out.println(a + " is bigger.");
+          }
+        else {
+            System.out.println(b + " is bigger.");
+          }
+  }
+}
