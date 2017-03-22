b2d703b30f285db605c330b53a14fe266bc6cf45
imifekete <imifekete@gmail.com>
2017-03-22 15:34:50 +0100

    exercise25

---
+++ week 02/day 03/src/exercise25.java
@@ -0,0 +1,40 @@
+/**
+ * Created by Fekete Imre on 2017.03.22..
+ */
+
+import java.util.Scanner;
+
+public class exercise25 {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Please give me a number!");
+    int ui1 = scanner.nextInt();
+    System.out.println("Please, give me another number!");
+    int ui2 = scanner.nextInt();
+
+    if (ui1 <= ui2) {
+      System.out.println("The second number should be bigger!");
+    } else {
+      for (int i = ui1; i > ui2; i--) {
+        System.out.println(i);
+      }
+    }
+
+  }
+}
+
+// Create a program that asks for two numbers
+// If the second number is not bigger than the first one it should print:
+// "The second number should be bigger"
+//
+// If it is bigger it should count from the first number to the second by one
+//
+// example:
+//
+// first number: 3, second number: 6, should print:
+//
+// 3
+// 4
+// 5
