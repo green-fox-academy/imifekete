46b01eed1cf7109551eb623f86158310a0631c32
imifekete <imifekete@gmail.com>
2017-03-22 15:30:38 +0100

    exercise24

---
+++ week 02/day 03/src/exercise24.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Fekete Imre on 2017.03.22..
+ */
+public class exercise24 {
+  public static void main(String[] args) {
+    // Create a program that prints all the even numbers between 0 and 500
+    for (int i = 0; i < 501; i += 2) {
+      System.out.println(i);
+    }
+  }
+}
