8ccf32be160b94f2ba9cacca63705f97454d7863
imifekete <imifekete@gmail.com>
2017-03-22 15:28:40 +0100

    exercise23

---
+++ week 02/day 03/src/exercise23.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Fekete Imre on 2017.03.22..
+ */
+public class exercise23 {
+  public static void main(String[] args) {
+    // Create a program that writes this line 100 times:
+    // "I won't cheat on the exam!"
+
+    for (int i = 0; i < 100; i++) {
+      System.out.println("I wont cheat on the exam!");
+    }
+  }
+}
