3afbbd129b97b1c7f39e0dfe473d64df797f5bcc
imifekete <imifekete@gmail.com>
2017-03-23 12:28:19 +0100

    third

---
+++ week 02/day 04/src/third.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Fekete Imre on 2017.03.23..
+ */
+public class third {
+  public static void main(String[] args) {
+
+    int[] q = {4, 5, 6, 7};
+
+    System.out.println(q[2]);
+  }
+}
+
+// - Create an array variable named `q`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print the third element of `q`
