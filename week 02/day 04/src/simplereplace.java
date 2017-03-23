7ba6e06331028e4d8100f6160a764a5510ab74eb
imifekete <imifekete@gmail.com>
2017-03-23 16:04:56 +0100

    simple_replace

---
+++ week 02/day 04/src/simplereplace.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Fekete Imre on 2017.03.23..
+ */
+public class simplereplace {
+  public static void main(String[] args) {
+    String example = "In a dishwasher far far away";
+
+    example = example.replace("dishwasher", "galaxy");
+
+    System.out.println(example);
+  }
+}
