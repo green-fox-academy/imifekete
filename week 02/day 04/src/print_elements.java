203a65ae71dc02ed2ba8c2fa29f1434815a085ca
imifekete <imifekete@gmail.com>
2017-03-23 14:08:40 +0100

    print_all_elements

---
+++ week 02/day 04/src/print_elements.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Fekete Imre on 2017.03.23..
+ */
+public class print_elements {
+  public static void main(String[] args) {
+
+    int [] af  = {4,5,6,7};
+
+    for (int num : af) {
+      System.out.print(num + " ");
+    }
+  }
+}
+
+// - Create an array variable named `af`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print all the elements of `af`
+
