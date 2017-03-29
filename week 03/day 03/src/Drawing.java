4706b9aaf1460d6b2496ac61a0bc00ad7e61f0ce
imifekete <imifekete@gmail.com>
2017-03-29 10:19:09 +0200

    Drawing lines

---
+++ week 03/day 03/src/Drawing.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Drawing {
+
+  public static void mainDraw(Graphics graphics){
+    graphics.setColor( Color.red);
+    graphics.drawLine(0, 300, 1000, 300);
+    graphics.setColor( Color.green);
+    graphics.drawLine (500, 0, 500, 1000);
+    //  draw a red horizontal line to the canvas' middle.
+    //  draw a green vertical line to the canvas' middle.
+
+
+
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
