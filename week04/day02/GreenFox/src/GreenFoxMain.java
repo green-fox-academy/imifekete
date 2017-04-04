daecb28d160ae0ad83c2bf44046c72f7e4625f66
imifekete <imifekete@gmail.com>
2017-04-04 12:30:29 +0200

    more student

--- week04/day02/GreenFox/src/GreenFoxMain.java
+++ week04/day02/GreenFox/src/GreenFoxMain.java
@@ -2,9 +2,17 @@
 public class GreenFoxMain {
   public static void main(String[] args) {
 
+    /*
     Person myPerson = new Person();
-    myPerson.introduce();
+    //myPerson.introduce();
 
     Student myStudent = new Student();
+    myStudent.introduce();
+    myStudent.skipDays(5);
+    myStudent.introduce();
+    */
+
+    Person myPerson = new Student();
+    myPerson.introduce();
   }
 }
