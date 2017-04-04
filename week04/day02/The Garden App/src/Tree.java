c6680a977af7513ab30229906423dc6a8d8eeceb
imifekete <imifekete@gmail.com>
2017-04-04 15:36:00 +0200

    tree

--- week04/day02/The Garden App/src/Tree.java
+++ week04/day02/The Garden App/src/Tree.java
@@ -1,5 +1,13 @@
 /**
  * Created by Fekete Imre on 2017.04.04..
  */
-public class Tree {
+public class Tree extends Plant {
+  public Tree (String color){
+    super (color, 10);
+  }
+
+  @Override
+  public void waterPlant (int water){
+    setCurrentWaterAmount(water * .4);
+  }
 }
