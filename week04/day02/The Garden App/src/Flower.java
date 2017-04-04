0e2d067fd4fdc1c35e24f0087e704125174dfa4e
imifekete <imifekete@gmail.com>
2017-04-04 15:31:37 +0200

    flower

--- week04/day02/The Garden App/src/Flower.java
+++ week04/day02/The Garden App/src/Flower.java
@@ -1,5 +1,13 @@
 /**
  * Created by Fekete Imre on 2017.04.04..
  */
-public class Flower {
+public class Flower extends Plant {
+  public Flower (String color){
+    super (color, 5);
+  }
+
+  @Override
+  public void waterPlant (int water){
+    setCurrentWaterAmount(water * .75);
+  }
 }
