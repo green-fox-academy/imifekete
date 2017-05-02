86b6d4e7ad692a06692dbfd9e9583980b722d85e
imifekete <imifekete@gmail.com>
2017-05-02 15:05:15 +0200

    counter added

--- week07/day02/task01/springstart/src/main/java/com/greenfoxacademy/controllers/HelloRESTController.java
+++ week07/day02/task01/springstart/src/main/java/com/greenfoxacademy/controllers/HelloRESTController.java
@@ -4,14 +4,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Created by Fekete Imre on 2017.05.02..
  */
 @RestController
 public class HelloRESTController {
+AtomicLong counter = new AtomicLong(1);
 
   @RequestMapping
   public Greeting greeting (@RequestParam("name") String name){
-    return new Greeting(1, name);
+    return new Greeting(counter.getAndIncrement(), "Hello, " + name);
   }
 }
