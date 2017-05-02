c80ba692c54d590871d261fb0fa4064c5457cc3b
imifekete <imifekete@gmail.com>
2017-05-02 17:07:39 +0200

    hello world web app

---
+++ week07/day02/task01/springstart/src/main/java/com/greenfoxacademy/controllers/HelloWebController.java
@@ -0,0 +1,17 @@
+package com.greenfoxacademy.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Fekete Imre on 2017.05.02..
+ */
+@Controller
+public class HelloWebController {
+  @RequestMapping("/web/greeting")
+  public String greeting(Model model) {
+    model.addAttribute("name", " World");
+    return "greeting";
+  }
+}
