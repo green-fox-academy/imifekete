c7da77cfc8c1ca965659e87845b6d1a1605769de
imifekete <imifekete@gmail.com>
2017-05-05 11:08:57 +0200

    route created

--- week07/day05/project/springstart/src/main/java/com/greenfoxproject/MainController.java
+++ week07/day05/project/springstart/src/main/java/com/greenfoxproject/MainController.java
@@ -1,7 +1,16 @@
 package com.greenfoxproject;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Fekete Imre on 2017.05.05..
  */
+@Controller
 public class MainController {
+
+  @RequestMapping ("/")
+    public String index(){
+    return "index";
+  }
 }
