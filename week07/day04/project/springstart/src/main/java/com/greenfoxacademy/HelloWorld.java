fbddf677ce4f7a8a7e875cdc989ffc64b952a6b2
imifekete <imifekete@gmail.com>
2017-05-05 10:38:50 +0200

    thursday stuff

---
+++ week07/day04/project/springstart/src/main/java/com/greenfoxacademy/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.greenfoxacademy;
+
+/**
+ * Created by Fekete Imre on 2017.05.04..
+ */
+public class HelloWorld {
+  private String message;
+
+  public void setMessage(String message){
+    this.message  = message;
+  }
+
+  public String getMessage(){
+    System.out.println("Your message: " + message);
+    return message;
+  }
+}
--- week07/day04/project/springstart/src/main/java/com/greenfoxacademy/HelloWorldApplictaion.java
+++ week07/day04/project/springstart/src/main/java/com/greenfoxacademy/HelloWorldApplictaion.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringstartApplication {
+class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringstartApplication.class, args);
+		SpringApplication.run(HelloWorldApplication.class, args);
 	}
+
 }
